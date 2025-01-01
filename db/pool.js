9df3360f9ae03ce2426cfc70058875b42d0bb564
BesarAjruli <aquila1912@gmail.com>
2025-01-01 18:08:34 +0100

    Made connection to outside datbase

--- db/pool.js
+++ db/pool.js
@@ -3,11 +3,7 @@ require('dotenv').config()
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    connectionString: process.env.DTABASE_URL
   });
 
   module.exports = pool
